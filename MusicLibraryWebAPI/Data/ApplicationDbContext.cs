8a7b93f3abb94b17fed12125127c79938cfdc9bb
Lewis Vine <lew.vine@gmail.com>
2021-03-30 12:31:33 -0400

    Initial commit with set-up per instructions complete

---
+++ MusicLibraryWebAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicLibraryWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicLibraryWebAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options)
+        {
+            public DbSet<Music> Music { get; set; }
+
+        }
+    }
+}
